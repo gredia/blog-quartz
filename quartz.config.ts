cee5cf4c2f64ef304b99589f7c2a8dc83eff64c0
gredia <89309898+gredia@users.noreply.github.com>
2026-01-22 22:31:44 +0900

    2026-01-22 22:31:44 blog content

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Noto Sans JP",
+        body: "Noto Sans JP",
+        code: "M PLUS 1 Code",
       },
       colors: {
         lightMode: {
